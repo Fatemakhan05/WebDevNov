918143d1cb7e87b0acaf944031bf81b963d1bdff
Fatemakhan05 <shahnk2004@gmail.com>
2021-12-02 14:02:35 +0100

    autocontroler

--- javascript/tuto5/app.js
+++ javascript/tuto5/app.js
@@ -9,15 +9,53 @@
 // message.innerHTML=`your username is ${getUserName.value }`
 // }
 // SayMyName()
-var number1=document.getElementById('num1')
-var number2=document.getElementById('num2')
-var getBtn = document.getElementById('btn')
-var result =document.getElementById('result')
+// var number1=document.getElementById('num1')
+// var number2=document.getElementById('num2')
+// var getBtn = document.getElementById('btn')
+// var result =document.getElementById('result')
 
-function DoCalc(){
-    console.log('hello test test')
-    var sumResult = Number( number1.value) + Number(number2.value)
-    console.log(sumResult)
-    result.innerHTML=`Result is ${sumResult}`
+// function DoCalc(){
+//     console.log('hello test test')
+//     var sumResult = Number( number1.value) + Number(number2.value)
+//     console.log(sumResult)
+//     result.innerHTML=`Result is ${sumResult}`
 
+// }
+//local variables and global variables
+//local variable
+// 
+//  const Auto= {
+//      a :function(){
+//          console.log('hello')
+//      },
+//      b:function(){
+//          console.log('hello 1')
+//      },
+//      getElementById :function(){
+//          console.log('getelementmethod')
+//      }
+//  }
+// const obj ={
+//     a=1,
+//     b:'string data',
+//     c=true,
+//     d:[1,2,3],
+//     e:{id:1,title:'this is title'},
+// }
+// console.log(obj.e)
+// obj.f()
+const result =document.getElementById('result')
+const AutoController={
+    // turnLeft: function(){result.innerHTML.log='turning left...'},
+    // turnRight: function(){console.log('turning right...')},
+    // backword:function(){console.log('backword')}
+    turnLeft: function(){result.innerHTML='turning left...'},
+     turnRight: function(){result.innerHTML='turning right...'},
+     backword:function(){result.innerHTML='backword'},
+     forword:function(){result.innerHTML='forword'},
+    
 }
+AutoController.turnRight()
+AutoController.turnLeft()
+AutoController.backword()
+AutoController.forword()
